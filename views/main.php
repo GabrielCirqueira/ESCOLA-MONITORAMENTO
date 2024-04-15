d21135abb83c86fd157387aa75e10ffac5b16985
Gabriel-Cirqueira <gabrielcirqueira711@gmail.com>
2024-04-15 08:42:05 -0300

    URL amigavel

--- views/main.php
+++ views/main.php
@@ -1,6 +1,6 @@
 <main class="main"> 
-    <a href="index.php?action=login_aluno" class="btn-index">ALUNO</a>
-    <a href="index.php?action=login_professor" class="btn-index">PROFESSOR</a>
-    <a href="index.php?action=login_gestor" class="btn-index">GESTOR</a>
-    <a href="index.php?action=login_adm" class="btn-index">ADM</a>
+    <a href="login_aluno" class="btn-index">ALUNO</a>
+    <a href="login_professor" class="btn-index">PROFESSOR</a>
+    <a href="login_gestor" class="btn-index">GESTOR</a>
+    <a href="login_adm" class="btn-index">ADM</a>
 </main>
