fde7944135b9989844bf8ce18111a54c54cd8804
OLD-gabriel <gabrielcirqueira711@gmail.com>
2024-09-04 22:13:53 -0300

    atualizações

---
+++ app/config/GetAlunos.php
@@ -0,0 +1,21 @@
+<?php 
+
+header("Content-Type: application/json");
+
+include "../../vendor/autoload.php";
+
+use Dotenv\Dotenv; 
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+use app\config\Database;
+
+$sql = "SELECT * FROM alunos";
+$result = Database::GetInstance()->query($sql);
+
+$dados = $result->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($dados);
+
+die();
